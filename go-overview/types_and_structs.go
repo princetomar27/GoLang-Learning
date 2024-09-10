c94b8793d842183dc1e5a3722df0e108f32a23e6
Prince Tomar <prince@farmsetu.co>
2024-09-10 08:15:08 +0530

    Updated types and structs files

--- go-overview/types_and_structs.go
+++ go-overview/types_and_structs.go
@@ -3,20 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 )
 
 var s = "Twenty One"
 
 type User struct {
-	Name string
-	Phone int
-	Age int
+	Name   string
+	Phone  int
+	Age    int
 	Gender string
-	Birth time.Time
 }
 
-func typesAndStructs (){
+func typesAndStructs() {
 	// fmt.Println("No is",s);
 	// var no2 = "Fifteen"
 	// fmt.Println("Second No. is",no2)
@@ -27,16 +25,16 @@ func typesAndStructs (){
 	var user2 User
 
 	user2 = User{
-		Name : "Tanisha Tomar",
-		Phone : 9191919192,
-		Age : 18,
+		Name:   "Tanisha Tomar",
+		Phone:  9191919192,
+		Age:    18,
 		Gender: "female",
 	}
-	
+
 	user := User{
-		Name : "Prince Tomar",
-		Phone : 9191919191,
-		Age : 21,
+		Name:   "Prince Tomar",
+		Phone:  9191919191,
+		Age:    21,
 		Gender: "male",
 	}
 	fmt.Println(user)
@@ -44,7 +42,7 @@ func typesAndStructs (){
 
 }
 
-func greet(s string ) (string,string){
+func greet(s string) (string, string) {
 	log.Println(s)
 	return s, "Prince"
 }
