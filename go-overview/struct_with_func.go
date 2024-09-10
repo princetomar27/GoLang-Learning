43eeee79724db7345643c1373763cc03716b0888
Prince Tomar <prince@farmsetu.co>
2024-09-10 08:37:24 +0530

    Structures with functions or extensions on structs

---
+++ go-overview/struct_with_func.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type myStruct struct {
+	Name string
+	Age  int
+}
+
+func (person *myStruct) printNameAndAge() (string, int) {
+	return "Your name is : " + person.Name + "\nAnd your age is", person.Age
+}
+
+func (person *myStruct) decreaseAge() int {
+	return person.Age - 10
+}
+
+func structWithFunc() {
+
+	var prince myStruct
+
+	prince.Name = "Prince"
+	prince.Age = 21
+
+	fmt.Println(prince)
+	fmt.Println(prince.printNameAndAge())
+	fmt.Println(prince.decreaseAge())
+	fmt.Println("Current Age is : ", prince.Age)
+}
+
+func main() {
+	structWithFunc()
+}
