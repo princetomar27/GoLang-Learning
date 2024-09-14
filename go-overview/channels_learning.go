d9cd60cea417f05a92413e9ec83ef54999d78f7e
Prince Tomar <tomarprince443@gmail.com>
2024-09-14 07:20:11 +0530

    Learnt about Go channels

--- go-overview/channels_learning.go
+++ go-overview/channels_learning.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"github.com/princetomar27/myfirstpackage/helpers"
+	"log"
+)
+
+const numberPool = 100
+
+func CalculateChanValue(intChan chan int) {
+	randomNumber := helpers.RandomNumber(numberPool)
+	intChan <- randomNumber
+}
+
+func main() {
+
+	intChan := make(chan int)
+
+	defer close(intChan)
+
+	go CalculateChanValue(intChan)
+
+	num := <-intChan
+
+	log.Println(num)
+
+}
--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,6 +1,20 @@
 package helpers
 
+import (
+	"math/rand"
+	"time"
+)
+
 type HelperType struct {
 	Typename string
 	TypeNum  int
+}
+
+func RandomNumber(n int) int {
+
+	rand.Seed(time.Now().UnixNano())
+
+	value := rand.Intn(n)
+
+	return value
 }
