128c9e4ec1b6c1940f346f1f965fe6553b1c93c6
Prince Tomar <tomarprince443@gmail.com>
2024-09-29 15:19:32 +0530

    Basic Middleware function intro

--- basic_web_application/cmd/web/chi_routes.go
+++ basic_web_application/cmd/web/chi_routes.go
@@ -13,6 +13,7 @@ func chiRoutes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
 
 	// Register the routes
 	mux.Get("/", handlers.Repo.Home)
